56f7585adef8a0a6db374858dc834172a65cae2c
Ailish <124047652+reyes-a@users.noreply.github.com>
2025-06-13 17:59:26 +0100

    Player Movement
    Force-based movement in all four directions based on how long the player holds the input

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float minForce = 1f;
     [SerializeField] float maxForce = 100f;
+    private float holdDuration;
+    [SerializeField] float incrementValue = 0.1f;
+
+    private bool isKeyDown = false;
 
     [SerializeField] Rigidbody rb; //It means rigidbody
 
@@ -18,25 +23,78 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Check if player wants to go forward (z)
-        if ( Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow) )
+        MovementInput(); //Calls the whole function
+
+        if (isKeyDown == true)
         {
-            rb.AddForce(0,0,maxForce);
-        }
-        //Check if player wants to go back (-z)
-        if ( Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow) )
-        {
-            rb.AddForce(0,0,-maxForce);
-        } 
-        //Check if player wants to go left (-x)
-        if ( Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow) )
-        {
-            rb.AddForce(-maxForce,0,0);
-        }
-        //Check if player wants to go right (x)
-        if ( Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow) )
-        {
-            rb.AddForce(maxForce,0,0);
+            holdDuration = Mathf.Clamp(holdDuration + (incrementValue * Time.deltaTime), 0f, 1f);
+            print(holdDuration);
+
+            if (holdDuration >= 1f)
+            {
+                isKeyDown = false;
+                print("SHUT UP");
+            }
         }
     }
+
+    /// <summary>
+    /// All the player movement stuff
+    /// </summary>
+    void MovementInput ()
+    {
+        //Check if player wants to go forward (z)
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            holdDuration = 0f;
+            isKeyDown = true;
+        }
+        //When player stops holding button, move
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            isKeyDown = false;
+            float force = Mathf.Lerp(minForce, maxForce, holdDuration);
+            rb.AddForce(0,0,force);
+        }
+
+        //(-z)
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            holdDuration = 0f;
+            isKeyDown = true;
+        }
+        if (Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            isKeyDown = false;
+            float force = Mathf.Lerp(minForce, maxForce, holdDuration);
+            rb.AddForce(0, 0, -force);
+        }
+
+        //(-x)
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            holdDuration = 0f;
+            isKeyDown = true;
+        }
+        if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            isKeyDown = false;
+            float force = Mathf.Lerp(minForce, maxForce, holdDuration);
+            rb.AddForce(-force, 0, 0);
+        }
+
+        //(x)
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            holdDuration = 0f;
+            isKeyDown = true;
+        }
+        if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            isKeyDown = false;
+            float force = Mathf.Lerp(minForce, maxForce, holdDuration);
+            rb.AddForce(force, 0, 0);
+        }
+    }
+
 }
